63c8492dca2d740fc6f2ae6c7ff275bfb0c06708
Erhan Bağdemir <erhan@reevik.net>
2026-09-11 11:56:22 +0300

    Project tab: use the folder-directory icon
    Swap the Project rail-tab icon for the file-directory SVG.

--- src/App.tsx
+++ src/App.tsx
@@ -2264,9 +2264,8 @@ function RailTab({ active, onClick, title, icon }: { active: boolean; onClick: (
 
 function FilesIcon() {
   return (
-    <svg viewBox="0 0 24 24" width="19" height="19" fill="none" stroke="currentColor" strokeWidth="1.7" strokeLinecap="round" strokeLinejoin="round">
-      <path d="M14 3H6a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z" />
-      <path d="M14 3v6h6" />
+    <svg viewBox="0 0 24 24" width="19" height="19" fill="currentColor">
+      <path fillRule="evenodd" clipRule="evenodd" d="M3.75 4.5a.25.25 0 00-.25.25v14.5c0 .138.112.25.25.25h16.5a.25.25 0 00.25-.25V7.687a.25.25 0 00-.25-.25h-8.471a1.75 1.75 0 01-1.447-.765L8.928 4.61a.25.25 0 00-.208-.11H3.75zM2 4.75C2 3.784 2.784 3 3.75 3h4.971c.58 0 1.12.286 1.447.765l1.404 2.063a.25.25 0 00.207.11h8.471c.966 0 1.75.783 1.75 1.75V19.25A1.75 1.75 0 0120.25 21H3.75A1.75 1.75 0 012 19.25V4.75z" />
     </svg>
   );
 }
